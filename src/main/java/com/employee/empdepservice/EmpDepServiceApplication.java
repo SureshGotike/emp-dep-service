616e5ac462ec8a54c57e688c216f355b521c0829
SXG6Wo7 <Samanvi123@>
2019-05-06 22:51:53 -0400

    employee department service - first commit

---
+++ src/main/java/com/employee/empdepservice/EmpDepServiceApplication.java
@@ -0,0 +1,13 @@
+package com.employee.empdepservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpDepServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpDepServiceApplication.class, args);
+	}
+
+}
